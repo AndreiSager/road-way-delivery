4c6d516921e00f4e136fe9b5c53f8564e54a5156
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-13 02:00:51 +0800

    Update Navbar.tsx

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import { NavLinks as links } from "@/constants"
 import { MdAccountCircle } from "react-icons/md"
 import { CgNotes } from "react-icons/cg"
-import { GiCarWheel } from "react-icons/gi"
+import { GiCarWheel, GiHamburgerMenu } from "react-icons/gi"
 
 export default function Navbar() {
     const sizeIcon = 30;
 
     return(
-        <nav className="flex flex-row justify-between items-center w-full p-6 ">
+        <nav className="flex flex-row justify-between items-center w-full py-4 px-12">
             <div className="flex flex-row gap-8">
                 <h1><a href="/" className="block p-4">Logo</a></h1>
-                <ul className="hidden md:flex flex-row">
+                <ul className="hidden lg:flex flex-row">
                     {links.map((link) => (
                         <li className="font-semibold text-lg">
                             <a href={link.href}
@@ -20,13 +20,15 @@ export default function Navbar() {
                     ))}
                 </ul>
             </div>
-
-            <div className="flex flex-row gap-8 justify-center items-center">
+            <div className="hidden lg:flex flex-row gap-8 justify-center items-center">
                 <div className="flex flex-row gap-4 justify-center items-center">
                     <MdAccountCircle size={sizeIcon}/>
                     <CgNotes size={sizeIcon}/>
                 </div>
-                <button type="button" className="px-12 py-4 bg-red-500 flex flex-row justify-center items-center gap-2"><GiCarWheel size={sizeIcon}/>Drive Now</button>
+                <button type="button" className="px-12 py-4 bg-red-500 flex flex-row justify-center items-center gap-2 text-white font-bold"><GiCarWheel size={sizeIcon}/>Drive Now</button>
+            </div>
+            <div className="flex lg:hidden">
+                <GiHamburgerMenu size={sizeIcon}/>
             </div>
         </nav>
     )

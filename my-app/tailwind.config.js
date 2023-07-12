783cc5c7eb89d90eb2b98a83e52f55def34befd3
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-13 02:36:11 +0800

    Update tailwind.config.js
    +Added a new color

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'accent-1': '#006600',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

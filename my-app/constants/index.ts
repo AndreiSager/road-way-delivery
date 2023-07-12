06b1363af12cec728cd7583fed069b78addc4140
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-13 03:54:43 +0800

    Update index.ts
    +Added links content

--- my-app/constants/index.ts
+++ my-app/constants/index.ts
@@ -16,6 +16,106 @@ export const FooterContent = {
     ],
 }
 
+export const FooterLinks = [
+    {
+        id: 1,
+        title:"Road Way Valley",
+        links: [
+            { href:"/", key:"About", title:"About" },
+            { href:"/", key:"Insider", title:"Insider" },
+            { href:"/", key:"Guides", title:"Guides" },
+            { href:"/", key:"Blog", title:"Blog" },
+            { href:"/", key:"Location", title:"Location" },
+            { href:"/", key:"Careers", title:"Careers" },
+            { href:"/", key:"Hiring", title:"Hiring" },
+            { href:"/", key:"Newsroom", title:"Newsroom" },
+            { href:"/", key:"Location Range", title:"Location Range" },
+            { href:"/", key:"Help Center", title:"Help Center" },
+        ]
+    },
+    {
+        id: 2,
+        title:"RWV Rider",
+        links: [
+            { href:"/", key:"Catch Up", title:"Catch Up" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Market", title:"RWV Biking" },
+            { href:"/", key:"RWV Market", title:"RWV Local" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+        ]
+    },
+    {
+        id: 3,
+        title:"RWV Delivery",
+        links: [
+            { href:"/", key:"Catch Up", title:"Catch Up" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+    {
+        id: 3,
+        title:"Drivers",
+        links: [
+            { href:"/", key:"Catch Up", title:"Catch Up" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+    {
+        id: 4,
+        title:"Commerce",
+        links: [
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+    {
+        id: 5,
+        title:"Corporation",
+        links: [
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+    {
+        id: 6,
+        title:"Terms and Policies",
+        links: [
+            { href:"/", key:"Catch Up", title:"Catch Up" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+    {
+        id: 7,
+        title:"Catch Up",
+        links: [
+            { href:"/", key:"Catch Up", title:"Catch Up" },
+            { href:"/", key:"RWV Food", title:"RWV Food" },
+            { href:"/", key:"RWV Market", title:"RWV Market" },
+            { href:"/", key:"RWV Express", title:"RWV Express" },
+            { href:"/", key:"RWV Premium", title:"RWV Premium" },
+        ]
+    },
+]
+
 export const DownloadLinks = [
     { storelink:"", key:"", title:""},
     { storelink:"", key:"", title:""},

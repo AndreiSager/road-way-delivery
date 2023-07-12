b3f3132c1a2c57670bf1b7d05ecaceb6ebb68153
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-13 04:36:33 +0800

    Created Routes

---
+++ my-app/app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function Pricing() {
+    return(
+        <main>
+            <h1>Pricing</h1>
+        </main>
+    )
+}
---
+++ my-app/app/testimonials/page.tsx
@@ -0,0 +1,7 @@
+export default function Testimonials() {
+    return(
+        <main>
+            <h1>Testimonials</h1>
+        </main>
+    )
+}

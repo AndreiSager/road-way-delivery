311fd827a3d0a83a92a796aa2634f6a8e7104590
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-14 03:53:43 +0800

    Update page.tsx

--- my-app/app/pricing/page.tsx
+++ my-app/app/pricing/page.tsx
@@ -1,7 +1,10 @@
-export default function Pricing() {
+import Pricing from "@/components/Pricing";
+
+export default function PricingPage() {
     return(
         <main>
             <h1>Pricing</h1>
+            <Pricing/>
         </main>
     )
 }

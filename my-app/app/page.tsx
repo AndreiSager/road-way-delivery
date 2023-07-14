f65a8b770721742f0f180d2747d0e4bb6c9ff572
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-14 08:27:37 +0800

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,12 @@
 import PerformanceBanner from '@/components/PerfomanceBanner'
+import Testimonials from '@/components/Testimonials'
 
 export default function Home() {
   return (
     <main className="flex flex-wrap">
       <h1>Home</h1>
       <PerformanceBanner/>
+      <Testimonials/>
     </main>
   )
 }

9fa4b246167ae5d87e3c2d19b309d5ef97cbe91b
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-16 04:22:43 +0800

    Created Hero Component

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from '@/components/Hero'
 import PerformanceBanner from '@/components/PerfomanceBanner'
 import Testimonials from '@/components/Testimonials'
 
 export default function Home() {
   return (
     <main className="flex flex-wrap">
-      <h1>Home</h1>
+      <Hero/>
       <PerformanceBanner/>
       <Testimonials/>
     </main>
---
+++ my-app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Hero() {
+    return(
+        <div className="flex flex-row flex-wrap px-0 py-16 lg:p-0">
+            <Image
+                src={"/hero_truck.svg"}
+                height={600}
+                width={700}
+                alt="White Truck"
+                className="order-2 lg:order-1 w-full lg:w-[58%]"
+                >
+            </Image>
+            <div className="order-1 lg:order-2 w-full lg:w-[42%] flex flex-col justify-center p-6 lg:p-12">
+                <h1 className="w-full md:w-[400px] font-bold text-5xl">Lorem ipsum dolor sit amet.</h1>
+                <p className="w-full md:w-[400px] font-medium text-slate-600 text-lg mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptates sit soluta aliquam laborum animi sit soluta aliquam.</p>
+                <button type="button" className="w-fit py-4 px-12 font-bold text-white bg-accent-1 rounded-full mt-6">Book Now!</button>
+            </div>
+        </div>
+    )
+}

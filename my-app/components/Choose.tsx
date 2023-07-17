0ee610ceb40bd405261be3009ff94c1338550d70
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-18 01:29:34 +0800

    Create Choose.tsx
    -Make this last

---
+++ my-app/components/Choose.tsx
@@ -0,0 +1,18 @@
+export default function Choose() {
+    return(
+        <div>
+            <h1>Choose your car</h1>
+            <div>
+                <h1>Selection</h1>  
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}

8f150397d58ac1704c4b35171e79e56fda6302e4
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-18 05:31:14 +0800

    Update Steps.tsx

--- my-app/components/Steps.tsx
+++ my-app/components/Steps.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 export default function Steps() {
     return(
         <div className="sectionPadding flex flex-col justify-center items-center gap-16">
-            <h1 className="font-bold text-4xl">Rent Now In Three Easy Steps</h1>
+            <h1 className="font-bold text-4xl">Get Delivered In Three Easy Steps</h1>
             <div className="flex flex-row flew-wrap justify-center items-center gap-4">
                 {StepsContent.map((steps) => (
                     <div className="p-6 flex flex-col w-[350px] aspect-square text-center items-center">
-                        <h1 className="font-bold text-2xl mb-6">{steps.title}</h1>
+                        <h1 className="font-bold text-2xl ">{steps.title}</h1>
                         <Image
                             src={steps.icon}
-                            height={200}
+                            height={100}
                             width={200}
                             alt={steps.alt}
-                            className="object-cover"
+                            className="object-cover my-12"
                             />
-                        <h1 className="font-bold text-lg mt-6">{steps.subtitle}</h1>
+                        <h1 className="font-bold text-lg">{steps.subtitle}</h1>
                         <p className="font-medium text-gray-500">{steps.description}</p>
                     </div>
                 ))}

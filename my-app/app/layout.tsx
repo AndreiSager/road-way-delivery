e32ff6ef352b488a436574655d25a4087d5ea045
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-19 07:25:06 +0800

    Update layout.tsx

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,8 @@
-import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import NewsletterBanner from '@/components/NewsletterBanner'
 import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default function RootLayout({
             <main className='w-full'>
               {children}
             </main>
+            <NewsletterBanner/>
             <Footer/>
           </div>
         </div>

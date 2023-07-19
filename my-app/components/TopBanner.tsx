e3ff2f7b64b258e109c54943320be01239aadd3d
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-19 10:24:26 +0800

    Update TopBanner.tsx

--- my-app/components/TopBanner.tsx
+++ my-app/components/TopBanner.tsx
@@ -7,8 +7,8 @@ export default function TopBanner() {
     const withoutSlash = pathname.replace('/','')
 
     return(
-        <div className='flex justify-center items-center w-full h-[300px] lg:h-[500px] bg-highway bg-cover bg-center'>
-            <div className='text-white capitalize text-center'>
+        <div className='sectionPadding flex justify-center items-center w-full bg-highway bg-cover bg-center'>
+            <div className='text-white capitalize text-center h-[300px] lg:h-[500px] flex flex-col justify-center items-center'>
                 <h1 className='font-bold text-6xl'>{withoutSlash}</h1>
                 <h1 className='font-bold text-xl'>home{pathname}</h1>
             </div>

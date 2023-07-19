f37e76f9b1be1256639c9de7b98cbce7ce41be35
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-20 06:58:44 +0800

    Update Hero.tsx

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Hero() {
     return(
         <div className="flex flex-row flex-wrap px-0 py-16 lg:p-0 bg-truck bg-cover w-full h-[100vh] bg-center lg:bg-right">
-            <div className="order-1 lg:order-2 w-full lg:w-[600px] flex flex-col justify-center p-6 lg:p-12 gap-8">
-                <div className="text-center lg:text-left">
+            <div className="order-1 lg:order-2 w-full lg:w-[800px] flex flex-col justify-center p-6 lg:p-12 gap-8">
+                <div className="text-center lg:text-left flex flex-col items-center lg:items-start ">
                     <h1 className="w-full md:w-[600px] font-bold text-3xl lg:text-5xl text-white uppercase">Where There's A Road, There's A Way.</h1>
                     <p className="w-full md:w-[600px] font-medium text-slate-200 text-lg mt-4">Embrace life's challenges with perseverance and unwavering belief in finding a way. With optimism and determination, unlock new opportunities and achieve your goals on this rewarding journey.</p>
                 </div>

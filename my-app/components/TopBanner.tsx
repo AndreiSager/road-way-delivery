a0929b21411511ee51bcf2bea840c0f6d33dfd83
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-19 09:29:06 +0800

    Update TopBanner.tsx

--- my-app/components/TopBanner.tsx
+++ my-app/components/TopBanner.tsx
@@ -4,10 +4,14 @@ import { usePathname } from 'next/navigation'
 
 export default function TopBanner() {
     const pathname = usePathname()
+    const withoutSlash = pathname.replace('/','')
 
     return(
-        <div>
-            <h1>{pathname}</h1>
+        <div className='flex justify-center items-center w-full h-[300px] lg:h-[500px] bg-highway bg-cover bg-center'>
+            <div className='text-white capitalize text-center'>
+                <h1 className='font-bold text-6xl'>{withoutSlash}</h1>
+                <h1 className='font-bold text-xl'>home{pathname}</h1>
+            </div>
         </div>
     )
 }

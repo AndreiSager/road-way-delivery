f009b917740a462524afc42b48e86d0d3c3b7017
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-20 06:58:42 +0800

    Update Navbar.tsx

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
 
     return(
         <nav className="w-full ">
-            <div id="navbar" className="flex flex-row justify-between items-center w-full py-4 px-4 md:px-12 shadow-md navbar__white fixed top-0 left-0 right-0 z-50 backdrop-blur-sm">
+            <div id="navbar" className="flex flex-row justify-between items-center w-full py-4 px-4 md:px-12 shadow-md navbar__white fixed top-0 left-auto right-auto z-50 backdrop-blur-sm max-w-screen-xl">
                 <div className="flex flex-row gap-8">
                     <Image 
                         src={logo}

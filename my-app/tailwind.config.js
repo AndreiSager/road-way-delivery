ea1adea2c02842383365afebc0fd87471f0c04a7
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-19 08:08:17 +0800

    Update tailwind.config.js

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'mountain-sunset': 'url(/mountain-sunset_compressed.jpg)',
       },
     },
   },

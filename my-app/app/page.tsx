22f7307103d71acd6e5d73eb826fabe558d4f269
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-21 06:48:43 +0800

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero'
 import PerformanceBanner from '@/components/PerfomanceBanner'
-import Booking from '@/components/BookingForm'
 import Testimonials from '@/components/Testimonials'
 import Steps from '@/components/Steps'
 import Models from '@/components/Models'

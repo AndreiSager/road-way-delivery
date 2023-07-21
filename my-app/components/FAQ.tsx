269b776a6369c2d3399fea2c1a3d37be97a14ea3
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-21 08:31:03 +0800

    Update FAQ.tsx

--- my-app/components/FAQ.tsx
+++ my-app/components/FAQ.tsx
@@ -7,13 +7,23 @@ export default function FAQ() {
                 <h1 className="sectionHeader">Frequently Asked Questions</h1>
                 <p className="font-medium text-gray-500">Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
             </div>
-            <div className='flex flex-col gap-8 '>
-                {Questions.map((question) => (
-                    <div key={question.id} className='flex flex-col gap-2 w-full lg:w-[800px] p-6 bg-white shadow-xl border-l-8 border-accent-1 rounded-xl'>
-                        <h1 className='font-bold text-lg lg:text-left'>{question.question}</h1>
-                        <p className='font-medium text-md text-slate-600 text-justify'>{question.answer}</p>
+            <div className='flex flex-col lg:flex-row gap-16 lg:p-8'>
+                <div className='flex flex-col gap-8 lg:order-2 w-full lg:w-[700px]'>
+                    {Questions.map((question) => (
+                        <div key={question.id} className='flex flex-col gap-2 w-full p-6 bg-white shadow-xl border-l-8 border-accent-1 rounded-xl'>
+                            <h1 className='font-bold text-lg lg:text-left'>{question.question}</h1>
+                            <p className='hidden font-medium text-md text-slate-600 text-justify'>{question.answer}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className='lg:order-1 flex flex-col gap-8'>
+                    <div className='flex flex-col gap-2 text-center lg:text-left'>
+                        <h6 className='font-medium text-xs text-slate-400 uppercase'>additional questions</h6>
+                        <h1 className='font-bold text-2xl'>Have any questions?</h1>
                     </div>
-                ))}
+                    <p className='font-medium text-sm text-slate-400 sentence text-justify w-full lg:w-[300px]'>We are an amicably reliable delivery service, focused on providing excellent customer service and timely deliveries to meet the needs of their clients. If you have any questions ask away and we will answer you worries within two business days.</p>
+                    <button type='button' className='capitalize callToAction'>ask away</button>
+                </div>
             </div>
         </div>
     )

0281047d6d4ca9ea2232e58e23d1fdd1bfbdca29
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-21 11:26:34 +0800

    Update PromotionalBanner.tsx

--- my-app/components/PromotionalBanner.tsx
+++ my-app/components/PromotionalBanner.tsx
@@ -1,8 +1,9 @@
 export default function PromotionalBanner() {
     return(
-        <div className="w-full h-[400px] lg:h-[250px] bg-stone-900 text-white flex flex-col justify-center items-center gap-4">
-            <h1 className="text-5xl font-bold">Lorem ipsum dolor sit amet consectetur.</h1>
-            <p className="text-xl font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, est?</p>
+        <div className="w-full h-[400px] lg:h-[250px] bg-stone-900 text-white flex flex-col justify-center items-center gap-6 text-center">
+              <p className="text-xl font-medium uppercase">food. gifts. products. furniture. people. you name it we deliver!</p>
+            <h1 className="text-3xl lg:text-5xl font-extrabold capitalize">where there's a <span className="text-green-600">road</span>, there's a <span className="text-green-600">way</span>!</h1>
+          
         </div>
     )
 }

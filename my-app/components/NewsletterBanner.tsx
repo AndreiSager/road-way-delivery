9a59be0c71aa9274086d7fb47c0ecc111275c52e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-21 12:05:03 +0800

    Update NewsletterBanner.tsx

--- my-app/components/NewsletterBanner.tsx
+++ my-app/components/NewsletterBanner.tsx
@@ -1,7 +1,7 @@
 export default function NewsletterBanner() {
     return(
-        <div className="w-full sectionPadding bg-mountain-sunset bg-cover lg:bg-contain bg-no-repeat bg-left-bottom lg:bg-right bg-primary-1 ">
-            <div className="w-full h-full lg:w-1/2 flex flex-col justify-center lg:mx-8 gap-4 text-center lg:text-left">
+        <div className="w-full sectionPadding bg-mountain-sunset bg-cover lg:bg-contain bg-no-repeat bg-center lg:bg-right bg-primary-1 ">
+            <div className="w-full h-full lg:w-1/2 flex flex-col justify-center lg:mx-8 gap-4 text-center lg:text-left lg:ml-24">
                 <div className="backdrop-brightness-50 lg:backdrop-brightness-100 flex flex-col gap-2">
                     <h1 className="font-bold text-3xl text-white">Join Our Newsletter</h1>
                     <p className="font-bold text-slate-200 w-full lg:w-[400px] ">Sign-up for our newsletter. Be the first one to receive the latest news from Road Way Delivery.</p>

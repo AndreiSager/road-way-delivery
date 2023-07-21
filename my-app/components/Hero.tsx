4bbd83b0a635f709abe86bf480225f7b0678124c
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-22 02:23:27 +0800

    Update Hero.tsx

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return(
-        <div className="flex flex-row flex-wrap px-0 py-16 lg:p-0 bg-truck bg-cover w-full h-[100vh] bg-center lg:bg-right">
+        <div className="flex flex-row flex-wrap px-0 py-16 lg:p-0 bg-truck bg-cover w-full h-[100vh] min-h-[1000px] max-h-[1000px] bg-center lg:bg-right">
             <div className="order-1 lg:order-2 w-full lg:w-[800px] flex flex-col justify-center lg:mt-32 p-6 lg:p-12 gap-8">
                 <div className="text-center lg:text-left flex flex-col items-center lg:items-start ">
                     <h1 className="w-full md:w-[600px] font-bold text-3xl lg:text-5xl text-white uppercase">Where There's A <span className="text-green-600">Road</span>, There's A <span  className="text-green-600">Way</span>.</h1>

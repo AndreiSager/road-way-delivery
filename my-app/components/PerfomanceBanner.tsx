5e3e104ae605a8073ab6e8032a05b4de419cb6b2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-22 03:46:48 +0800

    Update PerfomanceBanner.tsx

--- my-app/components/PerfomanceBanner.tsx
+++ my-app/components/PerfomanceBanner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function PerformanceBanner() {
     return(
-        <main className="w-full lg:h-[350px] bg-[url(/road_compressed.jpg)] bg-cover bg-bottom py-20 lg:py-0">
+        <main className="w-full lg:h-[350px] bg-slate-100 bg-cover bg-bottom py-20 lg:py-0">
             <div className="w-full h-full flex flex-row flex-wrap gap-10 justify-center items-center">
             {BannerContent.map((block) => (
                 <div key={block.id} className="w-[300px] md:w-[230px] aspect-square bg-white rounded-xl p-8 flex flex-row flex-wrap justify-center items-center text-center shadow-2xl hover:scale-105 linear duration-100 transition">

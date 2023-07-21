ad8b623683d18e7dac6f41ad7ba8c16958d5d9fd
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-22 03:55:29 +0800

    Update PerfomanceBanner.tsx

--- my-app/components/PerfomanceBanner.tsx
+++ my-app/components/PerfomanceBanner.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function PerformanceBanner() {
     return(
         <main className="w-full lg:h-[350px] bg-slate-100 bg-cover bg-bottom py-20 lg:py-0">
-            <div className="w-full h-full flex flex-row flex-wrap gap-10 justify-center items-center">
+            <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-0 place-content-center">
             {BannerContent.map((block) => (
-                <div key={block.id} className="w-[300px] md:w-[230px] aspect-square bg-white rounded-xl p-8 flex flex-row flex-wrap justify-center items-center text-center shadow-2xl hover:scale-105 linear duration-100 transition">
+                <div key={block.id} className="h-[180px] lg:w-[250px] aspect-video bg-white rounded-xl p-8 flex flex-row flex-wrap justify-center items-center text-center shadow-2xl hover:scale-105 linear duration-100 transition place-self-center">
                     <Image 
                         src={block.icon}
                         height={150}

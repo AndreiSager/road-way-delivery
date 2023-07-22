8de73558b29d506bccbd438ef254b8f28b2e9af2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-23 06:40:11 +0800

    Update FAQ.tsx
    +It works :)

--- my-app/components/FAQ.tsx
+++ my-app/components/FAQ.tsx
@@ -1,6 +1,47 @@
+"use client"
+
 import { FQAContent as Questions } from '@/constants'
+import { useEffect, useState } from 'react'
 
 export default function FAQ() {
+    const [accordion, setAccordion] = useState<boolean>(false);
+    const handleAccordion = () => {
+        setAccordion(!accordion);
+    };
+
+    useEffect(() => {
+        setState({ ...appState, activeObject: appState.objects[1] })
+    }, []);
+
+    type activeObjectProps = {
+        id: number;
+    }
+
+    const [appState, setState] = useState({
+        activeObject: { id: 1 },
+        objects: [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } ],
+    });
+    
+    function toggleActive(index: number) {
+        setState({ ...appState, activeObject: appState.objects[index] })
+    }
+    
+    function toggleActiveQuestion(index: number) {
+        if (appState.objects[index] === appState.activeObject) {
+            return 'cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg bg-accent-1 text-white'; 
+        } else {
+            return 'cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg';
+        }
+    }
+
+    function toggleActiveAnswer(index: number) {
+        if (appState.objects[index] === appState.activeObject) {
+            return 'rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150'; 
+        } else {
+            return 'rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150 absolute -top-[100%] -z-10';
+        }
+    }
+
     return(
         <div className="sectionPadding flex flex-col flex-wrap justify-center items-center gap-16 w-full bg-slate-100">
             <div className="flex flex-col w-full lg:w-[600px] text-center">
@@ -8,11 +49,11 @@ export default function FAQ() {
                 <p className="font-medium text-gray-500">Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
             </div>
             <div className='flex flex-col lg:flex-row gap-16 lg:p-8'>
-                <div className='flex flex-col gap-8 lg:order-2 w-full lg:w-[700px]'>
+                <div className='flex flex-col lg:order-2 w-full lg:w-[700px]'>
                     {Questions.map((question) => (
-                        <div key={question.id} className='flex flex-col gap-2 w-full p-6 bg-white shadow-xl border-l-8 border-accent-1 rounded-xl'>
-                            <h1 className='font-bold text-lg lg:text-left'>{question.question}</h1>
-                            <p className='font-medium text-md text-slate-600 text-justify'>{question.answer}</p>
+                        <div key={question.id} className='flex flex-col w-full bg-white border-t border-slate-300 relative overflow-hidden'>
+                            <h1 onClick={ () => {toggleActive(question.id)} } className={toggleActiveQuestion(question.id)}>{question.question}</h1>
+                            <p className={toggleActiveAnswer(question.id)}>{question.answer}</p>
                         </div>
                     ))}
                 </div>

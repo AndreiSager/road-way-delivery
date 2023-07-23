65a8b928e6c2976a2b1de2f728c85c5c76ac5e69
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-23 16:14:00 +0800

    Update Models.tsx
    +It now works

--- my-app/components/Models.tsx
+++ my-app/components/Models.tsx
@@ -2,8 +2,7 @@
 
 import { PricingPlans } from "@/constants";
 import Image from "next/image";
-import { useState } from "react";
-import { AiFillCar } from "react-icons/ai"
+import { useEffect, useState } from "react";
 import { FaGasPump } from "react-icons/fa";
 import { GiCarDoor, GiGearStick, GiWeight } from "react-icons/gi";
 import { IoLogoModelS } from "react-icons/io";
@@ -12,9 +11,75 @@ import { TbAirConditioning } from "react-icons/tb"
 export default function Models() {
     const iconSize = 30;
 
-    const [contentIndex, setContentIndex] = useState<number>(0);
-    const handleContentIndex = () => {
+    let [contentIndex, setContentIndex] = useState<number>(0);
+    const handleContentIndex = (index: number) => {
+        setContentIndex(index - 1);
+    }
 
+    type ModelProps = {
+        brand: string;
+        model: string;
+        year: string;
+        doors: number;
+        seats: number;
+        load: string;
+        ac: string;
+        transmission: string;
+        fuel: string;
+        price: number;
+    }
+
+    const [content, setContent] = useState<ModelProps>({
+        brand: PricingPlans[0].brand,
+        model: PricingPlans[0].model,
+        year: PricingPlans[0].year,
+        doors: PricingPlans[0].doors,
+        seats: PricingPlans[0].seats,
+        load: PricingPlans[0].load,
+        ac: PricingPlans[0].ac,
+        transmission: PricingPlans[0].transmission,
+        fuel: PricingPlans[0].fuel,
+        price: PricingPlans[0].price,
+    });
+
+    const handleContent = (contentIndex: number) => {
+        setContent({
+         brand: PricingPlans[contentIndex].brand,
+         model: PricingPlans[contentIndex].model,
+         year: PricingPlans[contentIndex].year,
+         doors: PricingPlans[contentIndex].doors,
+         seats: PricingPlans[contentIndex].seats,
+         load: PricingPlans[contentIndex].load,
+         ac: PricingPlans[contentIndex].ac,
+         transmission: PricingPlans[contentIndex].transmission,
+         fuel: PricingPlans[contentIndex].fuel,
+         price: PricingPlans[contentIndex].price,
+         })
+     }
+
+    useEffect(() => {
+        setState({ ...appState, activeObject: appState.objects[1] })
+    }, []);
+
+    useEffect(() => {
+        handleContent(contentIndex);
+    }, [handleContentIndex])
+
+    const [appState, setState] = useState({
+        activeObject: { id: 1 },
+        objects: [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 } ],
+    });
+    
+    function toggleActive(index: number) {
+        setState({ ...appState, activeObject: appState.objects[index] })
+    }
+    
+    function toggleActiveQuestion(index: number) {
+        if (appState.objects[index] === appState.activeObject) {
+            return "flex-none block p-4 w-full bg-accent-1 text-white hover:text-white font-bold text-lg rounded-lg hover:bg-accent-1 active:bg-accent-1 shadow-lg"; 
+        } else {
+            return "flex-none block p-4 w-full bg-slate-200 hover:text-white font-bold text-lg rounded-lg hover:bg-accent-1 active:bg-accent-1 shadow-lg";
+        }
     }
 
     return(
@@ -32,47 +97,50 @@ export default function Models() {
                     alt={PricingPlans[contentIndex].thumbnail}
                     />
                 <div className="w-full lg:w-2/5">
-                    <table className="w-full table-fixed border-separate border-spacing-2 lg:border-spacing-4 border border-1 border-slate-300 rounded-lg">
+                    <table className="w-full table-fixed border-separate border-spacing-2 lg:border-spacing-4 rounded-lg">
                         <thead>
                             <tr className="tableContainer">
                                 <td className="tableTitle">Brand</td>
-                                <td className="tableTitle">{PricingPlans[contentIndex].brand}</td>
+                                <td className="tableTitle">{content.brand}</td>
                             </tr>
                         </thead>
                         <tbody className="flex flex-col gap-2 lg:gap-4">
                             <tr className="tableContainer">
                                 <td className="tableContent"><IoLogoModelS size={iconSize}></IoLogoModelS>model</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].brand} {PricingPlans[contentIndex].model} {PricingPlans[contentIndex].year}</td>
+                                <td className="tableContent">{content.brand} {content.model} {content.year}</td>
                             </tr>
                             <tr className="tableContainer">
                                 <td className="tableContent"><GiCarDoor size={iconSize}></GiCarDoor>doors</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].doors}/{PricingPlans[contentIndex].seats}</td>
+                                <td className="tableContent">{content.doors}/{content.seats}</td>
                             </tr>
                             <tr className="tableContainer">
                                 <td className="tableContent"><GiWeight size={iconSize}></GiWeight>load</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].load}</td>
+                                <td className="tableContent">{content.load}</td>
                             </tr>      
                             <tr className="tableContainer">
                                 <td className="tableContent"><TbAirConditioning size={iconSize}></TbAirConditioning>ac</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].ac}</td>
+                                <td className="tableContent">{content.ac}</td>
                             </tr>
                             <tr className="tableContainer">
                                 <td className="tableContent"><GiGearStick size={iconSize}></GiGearStick>transmission</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].transmission}</td>
+                                <td className="tableContent">{content.transmission}</td>
                             </tr>
                             <tr className="tableContainer">
                                 <td className="tableContent"><FaGasPump size={iconSize}></FaGasPump>fuel</td>
-                                <td className="tableContent">{PricingPlans[contentIndex].fuel}</td>
+                                <td className="tableContent">{content.fuel}</td>
                             </tr>
                         </tbody>
                     </table>
-                    <button type="button" className="callToAction mt-6 uppercase shadow-lg">${PricingPlans[contentIndex].price} / booking</button>
+                    <button type="button" className="callToAction mt-6 uppercase shadow-lg">${content.price} / booking</button>
                 </div>
             </div>
             <div className="order-2 lg:order-3 grid grid-cols-2 lg:grid-cols-4 gap-4 lg:p-8">
                 {PricingPlans.map((content) => (
-                    <div key={content.id} className="">
-                        <h1 className="flex-none block p-4 w-full bg-slate-200 hover:text-white font-bold text-lg rounded-lg hover:bg-accent-1 active:bg-accent-1 shadow-lg">{content.name}</h1>
+                    <div key={content.id} 
+                        >
+                        <h1 onClick={ () => { toggleActive(content.id); handleContentIndex(content.id)  } } 
+                            className={toggleActiveQuestion(content.id)}
+                        >{content.name}</h1>
                     </div>
                 ))}
             </div>

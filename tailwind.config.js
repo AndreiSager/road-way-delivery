81a58bc1dac934183d579b260edfa7bb07f6edf9
Andrei <andreiwork25@gmail.com>
2024-10-02 14:59:00 +0800

    Update tailwind.config.js
    Added maxWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary-1': '#A06C20',
-        'accent-1': '#16a34a',
+        "primary-1": "#A06C20",
+        "accent-1": "#16a34a",
+      },
+      maxWidth: {
+        center: "1980px",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'mountain-sunset': 'url(/mountain-sunset_compressed.jpg)',
-        'highway': 'url(/highway-dark_compressed.jpg)',
-        'truck': 'url(/hero-truck_dark.jpg)'
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "mountain-sunset": "url(/mountain-sunset_compressed.jpg)",
+        highway: "url(/highway-dark_compressed.jpg)",
+        truck: "url(/hero-truck_dark.jpg)",
       },
     },
   },
   plugins: [],
-}
+};

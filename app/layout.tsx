defb3974edf53cbb61eb3c3ef2f8e247b3d8fc80
Andrei <andreiwork25@gmail.com>
2024-10-02 14:59:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import NewsletterBanner from '@/components/NewsletterBanner'
-import Footer from '@/components/Footer'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import NewsletterBanner from "@/components/NewsletterBanner";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Road Way Delivery',
-  description: 'A create next app that only features front-end functionality.',
-}
+  title: "Road Way Delivery",
+  description: "A create next app that only features front-end functionality.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='flex justify-center'>
-          <div className='flex flex-wrap w-full max-w-screen-xl'>
-            <Navbar/>
-            <main className='w-full'>
-              {children}
-            </main>
-            <NewsletterBanner/>
-            <Footer/>
+        <div className="flex w-full justify-center">
+          <div className="flex w-full flex-col">
+            <Navbar />
+            <main className="w-full">{children}</main>
+            <NewsletterBanner />
+            <Footer />
           </div>
         </div>
       </body>
     </html>
-  )
+  );
 }

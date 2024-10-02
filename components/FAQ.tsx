4f4c8ad6b11bfd69cb663cbc9de9ebf9dd60b337
Andrei <andreiwork25@gmail.com>
2024-10-02 18:08:14 +0800

    Update FAQ.tsx

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,62 +1,101 @@
-"use client"
+"use client";
 
-import { FQAContent as Questions } from '@/constants'
-import { useEffect, useState } from 'react'
+import { FQAContent as Questions } from "@/constants";
+import { useEffect, useState } from "react";
+import { ChatCircle, House03, QrCode } from "react-coolicons";
 
 export default function FAQ() {
-    useEffect(() => {
-        setState({ ...appState, activeObject: appState.objects[1] })
-    }, []);
+  useEffect(() => {
+    setState({ ...appState, activeObject: appState.objects[1] });
+  }, []);
 
-    const [appState, setState] = useState({
-        activeObject: { id: 1 },
-        objects: [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } ],
-    });
-    
-    function toggleActive(index: number) {
-        setState({ ...appState, activeObject: appState.objects[index] })
-    }
-    
-    function toggleActiveQuestion(index: number) {
-        if (appState.objects[index] === appState.activeObject) {
-            return 'cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg bg-accent-1 text-white'; 
-        } else {
-            return 'cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg hover:bg-accent-1 hover:text-white';
-        }
-    }
+  const [appState, setState] = useState({
+    activeObject: { id: 1 },
+    objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+  });
 
-    function toggleActiveAnswer(index: number) {
-        if (appState.objects[index] === appState.activeObject) {
-            return 'rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150'; 
-        } else {
-            return 'rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150 absolute -top-[100%] -z-10';
-        }
-    }
+  function toggleActive(index: number) {
+    setState({ ...appState, activeObject: appState.objects[index] });
+  }
 
-    return(
-        <div className="sectionPadding flex flex-col flex-wrap justify-center items-center gap-16 w-full bg-slate-100">
-            <div className="flex flex-col w-full lg:w-[600px] text-center">
-                <h1 className="sectionHeader">Frequently Asked Questions</h1>
-                <p className="font-medium text-gray-500">Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
+  function toggleActiveQuestion(index: number) {
+    if (appState.objects[index] === appState.activeObject) {
+      return "cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg bg-accent-1 text-white";
+    } else {
+      return "cursor-pointer font-bold text-lg lg:text-left block p-6 shadow-xl rounded-lg hover:bg-accent-1 hover:text-white";
+    }
+  }
+
+  function toggleActiveAnswer(index: number) {
+    if (appState.objects[index] === appState.activeObject) {
+      return "rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150";
+    } else {
+      return "rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150 absolute -top-[100%] -z-10";
+    }
+  }
+
+  return (
+    <div className="flex w-full flex-col flex-wrap items-center justify-center gap-16 px-40 py-24">
+      <div className="flex w-full flex-col text-center lg:w-[600px]">
+        <h1 className="text-5xl font-medium text-black">
+          Frequently Asked Questions
+        </h1>
+        <p className="font-medium text-gray-500">
+          Discover the positive impact we've made on the our clients by reading
+          through their testimonials. Our clients have experienced our service
+          and results, and they're eager to share their positive experiences
+          with you.
+        </p>
+      </div>
+      <div className="flex flex-col gap-40 lg:flex-row lg:p-8">
+        <div className="flex flex-col gap-24">
+          {Categories.map(({ id, title, icon }) => (
+            <div key={id} className="flex flex-row items-center gap-4">
+              <div className="h-10 w-10">{icon}</div>
+              <p className="text-2xl font-bold">{title}</p>
             </div>
-            <div className='flex flex-col lg:flex-row gap-16 lg:p-8'>
-                <div className='flex flex-col lg:order-2 w-full lg:w-[700px]'>
-                    {Questions.map((question) => (
-                        <div key={question.id} className='flex flex-col w-full bg-white border-t border-slate-300 relative overflow-hidden'>
-                            <h1 onClick={ () => {toggleActive(question.id)} } className={toggleActiveQuestion(question.id)}>{question.question}</h1>
-                            <p className={toggleActiveAnswer(question.id)}>{question.answer}</p>
-                        </div>
-                    ))}
-                </div>
-                <div className='lg:order-1 flex flex-col gap-8'>
-                    <div className='flex flex-col gap-2 text-center lg:text-left'>
-                        <h6 className='font-medium text-xs text-slate-400 uppercase'>additional questions</h6>
-                        <h1 className='font-bold text-2xl'>Have any questions?</h1>
-                    </div>
-                    <p className='font-medium text-sm text-slate-400 sentence text-justify w-full lg:w-[300px]'>We are an amicably reliable delivery service, focused on providing excellent customer service and timely deliveries to meet the needs of their clients. If you have any questions ask away and we will answer you worries within two business days.</p>
-                    <button type='button' className='capitalize callToAction'>ask away</button>
-                </div>
-            </div>
+          ))}
         </div>
-    )
+        <div className="rounded-ful; hidded h-full w-[1px] bg-gray-200 md:flex" />
+        <div className="flex w-full flex-col lg:w-[700px]">
+          {Questions.map((question) => (
+            <div
+              key={question.id}
+              className="relative flex w-full flex-col overflow-hidden border-t border-slate-300 bg-white"
+            >
+              <h1
+                onClick={() => {
+                  toggleActive(question.id);
+                }}
+                className={toggleActiveQuestion(question.id)}
+              >
+                {question.question}
+              </h1>
+              <p className={toggleActiveAnswer(question.id)}>
+                {question.answer}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const Categories = [
+  {
+    id: 0,
+    title: "General",
+    icon: <House03 className="h-10 w-10" />,
+  },
+  {
+    id: 1,
+    title: "Support",
+    icon: <ChatCircle className="h-10 w-10" />,
+  },
+  {
+    id: 2,
+    title: "Others",
+    icon: <QrCode className="h-10 w-10" />,
+  },
+];

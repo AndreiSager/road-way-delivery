6a511d6dc3b81321d85cdd7e1d7b0f817af5966e
Andrei <andreiwork25@gmail.com>
2024-10-02 17:17:01 +0800

    Update NewsletterBanner.tsx

--- components/NewsletterBanner.tsx
+++ components/NewsletterBanner.tsx
@@ -5,8 +5,8 @@ export default function NewsletterBanner() {
         <div className="relative h-[600px] w-full max-w-center overflow-hidden rounded-[3rem] bg-mountain-sunset bg-cover bg-right bg-no-repeat">
           <div className="absolute inset-0 z-0 h-full w-full bg-gradient-to-r from-black from-30% to-transparent to-70% opacity-40" />
 
-          <div className="z-10 flex h-full w-full flex-col items-start justify-center gap-4 text-center lg:mx-8 lg:ml-20 lg:w-1/2 lg:text-left">
-            <div className="z-10 flex flex-col gap-2 backdrop-brightness-50 lg:backdrop-brightness-100">
+          <div className="z-10 flex h-full w-full flex-col items-start justify-center gap-12 text-center lg:mx-8 lg:ml-20 lg:w-1/2 lg:text-left">
+            <div className="z-10 flex flex-col gap-4 backdrop-brightness-50 lg:backdrop-brightness-100">
               <h1 className="z-10 text-4xl font-bold text-white">
                 Join Our Newsletter
               </h1>
@@ -17,17 +17,17 @@ export default function NewsletterBanner() {
             </div>
             <form
               action=""
-              className="z-10 mt-4 flex w-full flex-col items-center gap-2 lg:flex-row"
+              className="z-10 flex w-full flex-col items-center gap-2 lg:flex-row"
             >
               <input
                 type="email"
                 name="email"
                 placeholder="Enter your email..."
-                className="inputField w-full max-w-[500px] focus:placeholder:italic"
+                className="inputField w-full max-w-[480px] focus:placeholder:italic"
               />
               <button
                 type="button"
-                className="callToAction mt-2 w-full max-w-[200px] flex-auto shadow-xl lg:mt-0"
+                className="h-full w-full max-w-[200px] rounded-lg bg-accent-1 px-12 text-base font-bold text-white"
               >
                 Join Now
               </button>

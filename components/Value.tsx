4948b85f7e87999d3b5bec32bc8c7ebd2241c81c
Andrei <andreiwork25@gmail.com>
2024-10-03 20:56:51 +0800

    Update Value.tsx

--- components/Value.tsx
+++ components/Value.tsx
@@ -1,45 +1,50 @@
-import { ValueContent as content } from "@/constants";
-import Image from "next/image";
+import { FaCheckCircle } from "react-icons/fa";
+import DownloadBtns from "./Downloads/DownloadBtns";
 
 export default function Value() {
-    return(
-        <div className="sectionPadding w-full flex flex-col lg:flex-row bg-slate-100 gap-16 lg:px-16">
-            <div className="w-full lg:w-1/2 flex flex-col gap-8">
-                <Image
-                    src={content.image}
-                    width={520}
-                    height={300}
-                    alt={content.alt}
-                    className="rounded-lg"
-                    />
-                <div className="flex flex-col gap-6">
-                    <div className="flex flex-col gap-2">
-                        <h6 className="font font-medium uppercase text-slate-500">{content.overline}</h6>
-                        <h1 className="font-bold text-3xl capitalize">{content.title}</h1>
-                    </div>
-                    <p className="font font-medium sentence text-slate-500">{content.description}</p>
-                    <button type="button" className="callToAction lg:w-[200px]">{content.cta}</button>
-                </div>
+  const size = 20;
+
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <div className="flex w-full max-w-center flex-row items-center justify-between px-40 py-24">
+        <div className="flex w-full max-w-[730px] flex-col gap-12">
+          <h1 className="text-5xl font-bold">
+            Your All-in-One Solution for Fast Deliveries
+          </h1>
+          <div className="flex flex-col gap-6">
+            <p className="w-full max-w-[570px]">
+              Roadway Delivery offers real-time tracking, comprehensive delivery
+              analytics, and a robust set of features, streamlining your
+              logistics and providing instant access to all your critical data.
+            </p>
+            <div className="">
+              <div className="flex w-full max-w-[510px] flex-row items-center gap-2">
+                <FaCheckCircle size={size} className="text-accent-1" />
+                <p className="">
+                  Schedule and monitor deliveries from anywhere
+                </p>
+              </div>
+              <div className="flex w-full max-w-[510px] flex-row items-center gap-2">
+                <FaCheckCircle size={size} className="text-accent-1" />
+                <p className=""> Get instant pricing and real-time tracking </p>
+              </div>
+              <div className="flex w-full max-w-[510px] flex-row items-center gap-2">
+                <FaCheckCircle size={size} className="text-accent-1" />
+                <p className=""> Review delivery history and performance</p>
+              </div>
+              <div className="flex w-full max-w-[510px] flex-row items-center gap-2">
+                <FaCheckCircle size={size} className="text-accent-1" />
+                <p className="">
+                  {" "}
+                  Track delivery speed, success rates, and routes
+                </p>
+              </div>
             </div>
-            <div className="w-full lg:w-1/2 flex flex-col gap-8">
-                {content.feature.map((content) => (
-                    <div key={content.id} className="flex flex-row justify-center items-center w-full">
-                        <div className="rounded-full border-2 border-accent-1 min-w-[150px] aspect-square flex flex-col justify-center items-center z-10 overflow-hidden bg-white hover:scale-105 ease-out duration-150">
-                            <Image
-                            src={content.icon}
-                            width={100}
-                            height={100}
-                            alt={content.alt}
-                            className="object-cover"
-                            />
-                        </div>
-                        <div className="rounded-r-full h-[120px] flex flex-col justify-center py-10 pl-12 pr-6 -ml-8 shadow-xl bg-white">
-                            <h1 className="font-bold text-md capitalize">{content.title}</h1>
-                            <p className="font-medium text-xs lg:text-sm text-slate-500 sentence">{content.sentence}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+          </div>
+          <DownloadBtns />
         </div>
-    )
+        <div>Place Mockup Here</div>
+      </div>
+    </div>
+  );
 }

ffb761254f02342350485c3ecc66452d15416df2
Andrei <andreiwork25@gmail.com>
2024-10-03 16:39:14 +0800

    Update FAQ.tsx

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -2,7 +2,7 @@
 
 import { FQAGeneral, FQASupport, FQAOthers } from "@/constants";
 import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   ChatCircle,
   ChevronDown,
@@ -11,49 +11,7 @@ import {
 } from "react-coolicons";
 
 export default function FAQ() {
-  useEffect(() => {
-    setState({ ...appState, activeObject: appState.objects[1] });
-  }, []);
-
-  const [appState, setState] = useState({
-    activeObject: { id: 1 },
-    objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
-  });
-
-  function toggleActive(index: number) {
-    setState({ ...appState, activeObject: appState.objects[index] });
-  }
-
-  function toggleActiveQuestion(index: number) {
-    if (appState.objects[index] === appState.activeObject) {
-      return "cursor-pointer font-bold text-lg lg:text-left block p-6 rounded-lg text-accent-1 shadow-xl";
-    } else {
-      return "cursor-pointer font-bold text-lg lg:text-left block p-6  rounded-lg hover:text-accent-1";
-    }
-  }
-
-  function toggleActiveAnswer(index: number) {
-    if (appState.objects[index] === appState.activeObject) {
-      return "rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150 ";
-    } else {
-      return "rounded-lg font-medium text-md text-slate-600 text-justify block p-6 ease-in-out duration-150 absolute -top-[100%] -z-10";
-    }
-  }
-
-  // const [category, setCategory] = useState("General");
-  const [questions, setQuestions] = useState(FQAGeneral);
-  const handleQuestions = (title: string) => {
-    switch (title) {
-      case "General":
-        setQuestions(FQAGeneral);
-      case "Support":
-        setQuestions(FQASupport);
-      case "Others":
-        setQuestions(FQAOthers);
-      default:
-        setQuestions(FQAGeneral);
-    }
-  };
+  const [category, setCategory] = useState("General");
 
   return (
     <div className="flex w-full flex-col flex-wrap items-center justify-center gap-16 px-40 py-24">
@@ -74,26 +32,63 @@ export default function FAQ() {
             <button
               key={id}
               className="flex select-none flex-row items-center gap-4"
-              onClick={() => handleQuestions(title)}
+              onClick={() => setCategory(title)}
             >
-              <div className="h-10 w-10">{icon}</div>
-              <p className="text-2xl font-bold">{title}</p>
+              {title === category ? (
+                <>
+                  <div className="h-10 w-10 text-accent-1">{icon}</div>
+                  <p className="text-xl font-bold text-accent-1">{title}</p>
+                </>
+              ) : (
+                <>
+                  <div className="h-10 w-10">{icon}</div>
+                  <p className="text-xl font-bold">{title}</p>
+                </>
+              )}
             </button>
           ))}
         </div>
         <div className="hidden h-full w-[1px] rounded-full bg-gray-200 md:flex" />
 
-        <Accordion allowMultiple={false} transition transitionTimeout={500}>
-          {questions.map((question, id) => (
-            <AccordionItem
-              header={question.question}
-              key={id}
-              initialEntered={id === 0 ? true : false}
-            >
-              {question.answer}
-            </AccordionItem>
-          ))}
-        </Accordion>
+        {category === "General" && (
+          <Accordion allowMultiple={false} transition transitionTimeout={500}>
+            {FQAGeneral.map((question, id) => (
+              <AccordionItem
+                header={question.question}
+                key={id}
+                initialEntered={id === 0 ? true : false}
+              >
+                {question.answer}
+              </AccordionItem>
+            ))}
+          </Accordion>
+        )}
+        {category === "Support" && (
+          <Accordion allowMultiple={false} transition transitionTimeout={500}>
+            {FQASupport.map((question, id) => (
+              <AccordionItem
+                header={question.question}
+                key={id}
+                initialEntered={id === 0 ? true : false}
+              >
+                {question.answer}
+              </AccordionItem>
+            ))}
+          </Accordion>
+        )}
+        {category === "Others" && (
+          <Accordion allowMultiple={false} transition transitionTimeout={500}>
+            {FQAOthers.map((question, id) => (
+              <AccordionItem
+                header={question.question}
+                key={id}
+                initialEntered={id === 0 ? true : false}
+              >
+                {question.answer}
+              </AccordionItem>
+            ))}
+          </Accordion>
+        )}
       </div>
     </div>
   );

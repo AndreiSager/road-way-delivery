e910928fc55137a64dba427f257f4468bcfbec6a
Andrei <andreiwork25@gmail.com>
2024-10-03 20:40:31 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,17 @@ import Hero from "@/components/Hero";
 import PromotionalBanner from "@/components/PromotionalBanner";
 import Testimonials from "@/components/Testimonials";
 import Steps from "@/components/Steps";
-import Models from "@/components/Models";
 import FAQ from "@/components/FAQ";
 import Value from "@/components/Value";
 import Services from "@/components/Services";
 
 export default function Home() {
   return (
-    <main className="flex flex-wrap">
+    <main className="flex flex-col">
       <Hero />
       <PromotionalBanner />
       <Services />
       <Steps />
-      <Models />
       <Value />
       <Testimonials />
       <FAQ />

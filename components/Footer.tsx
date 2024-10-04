767cca8b4263942b6708f35143a0d414578e772a
Andrei <andreiwork25@gmail.com>
2024-10-04 20:04:29 +0800

    Refactored footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,8 @@
-import { FooterLinks, FooterContent } from "@/constants";
 import Image from "next/image";
-import {
-  BsFacebook,
-  BsYoutube,
-  BsTwitter,
-  BsTiktok,
-  BsMailbox,
-  BsTelephone,
-} from "react-icons/bs";
-import { FaInstagramSquare } from "react-icons/fa";
-import { GoGear } from "react-icons/go";
 import Icon from "@svgr-iconkit/entypo";
+import { FooterContent, FooterLinksData } from "@/constants";
 
 export default function Footer() {
-  const sizeIcon = 30;
-
   return (
     <footer className="flex w-full flex-col items-center">
       <div className="spacing-horizontal spacing-vertical flex w-full max-w-center flex-col items-center justify-center gap-24">
@@ -57,50 +45,50 @@ export default function Footer() {
           <div className="grid w-full grid-cols-2 gap-8 md:grid-cols-8 md:gap-12 lg:w-fit">
             <div className="row-span-2">
               <FooterColumn
-                title={FooterLinks[0].title}
-                links={FooterLinks[0].links}
+                title={FooterLinksData[0].title}
+                links={FooterLinksData[0].links}
               />
             </div>
             <div className="row-span-2">
               <FooterColumn
-                title={FooterLinks[1].title}
-                links={FooterLinks[1].links}
+                title={FooterLinksData[1].title}
+                links={FooterLinksData[1].links}
               />
             </div>
             <div className="row-span-1 row-start-3 md:row-span-2 md:row-start-auto">
               <FooterColumn
-                title={FooterLinks[2].title}
-                links={FooterLinks[2].links}
+                title={FooterLinksData[2].title}
+                links={FooterLinksData[2].links}
               />
             </div>
             <div className="row-span-1 row-start-4 md:row-span-2 md:row-start-auto">
               <FooterColumn
-                title={FooterLinks[4].title}
-                links={FooterLinks[4].links}
+                title={FooterLinksData[4].title}
+                links={FooterLinksData[4].links}
               />
             </div>
             <div className="row-span-2">
               <FooterColumn
-                title={FooterLinks[3].title}
-                links={FooterLinks[3].links}
+                title={FooterLinksData[3].title}
+                links={FooterLinksData[3].links}
               />
             </div>
             <div className="row-span-2">
               <FooterColumn
-                title={FooterLinks[6].title}
-                links={FooterLinks[6].links}
+                title={FooterLinksData[6].title}
+                links={FooterLinksData[6].links}
               />
             </div>
             <div className="col-start-2 row-span-1 row-start-5 md:col-start-auto md:row-span-2 md:row-start-auto">
               <FooterColumn
-                title={FooterLinks[5].title}
-                links={FooterLinks[5].links}
+                title={FooterLinksData[5].title}
+                links={FooterLinksData[5].links}
               />
             </div>
             <div className="col-start-2 row-span-1 row-start-6 md:col-start-auto md:row-span-2 md:row-start-auto">
               <FooterColumn
-                title={FooterLinks[7].title}
-                links={FooterLinks[7].links}
+                title={FooterLinksData[7].title}
+                links={FooterLinksData[7].links}
               />
             </div>
           </div>
@@ -114,14 +102,11 @@ export default function Footer() {
 }
 
 type LinksProps = {
-  id: number;
   href: string;
-  key: string;
   title: string;
 };
 
 type ColumnProps = {
-  id?: number;
   title: string;
   links: Array<LinksProps>;
 };
@@ -129,15 +114,17 @@ type ColumnProps = {
 const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="flex w-full flex-col gap-4 md:max-w-none">
     <h1 className="text-sm font-bold uppercase">{title}</h1>
-    <ul className="flex flex-col gap-2">
-      {links.map((link) => (
-        <li
-          key={link.id}
-          className="text-xs font-medium text-gray-500 hover:text-accent-1 hover:underline"
+    <div className="flex flex-col gap-2">
+      {links.map(({ href, title }, id) => (
+        <a
+          key={id}
+          href={href}
+          target="_blank"
+          className="text-xs font-medium text-gray-500 visited:text-accent-1 hover:text-accent-1 hover:underline"
         >
-          <a href={link.href}>{link.title}</a>
-        </li>
+          {title}
+        </a>
       ))}
-    </ul>
+    </div>
   </div>
 );
---
+++ constants/FooterLinksDB.tsx
@@ -0,0 +1,131 @@
+export const FooterLinksData = [
+  {
+    title: "About Us",
+    links: [
+      {
+        href: "/#about",
+        title: "About",
+      },
+      { href: "/#insider", title: "Insider" },
+      { href: "/#guides", title: "Guides" },
+      { href: "/#blog", title: "Blog" },
+      { href: "/#locations", title: "Locations" },
+      { href: "/#careers", title: "Careers" },
+      { href: "/#hiring", title: "Hiring" },
+      { href: "/#newsroom", title: "Newsroom" },
+      {
+        href: "/#rw-impact",
+        title: "Road Way Impact",
+      },
+      { href: "/#no-link", title: "Help Center" },
+    ],
+  },
+  {
+    title: "Services",
+    links: [
+      { href: "/#catchup-services", title: "Catch Up" },
+      { href: "/#rwd-guides", title: "RWD Guides" },
+      { href: "/#rwd-market", title: "RWD Market" },
+      { href: "/#rwd-biking", title: "RWD Biking" },
+      { href: "/#rwd-local", title: "RWD Local" },
+      { href: "/#rwd-express", title: "RWD Express" },
+      { href: "/#rwd-movers", title: "RWD Movers" },
+      { href: "/#rwd-premium", title: "RWD Premium" },
+      { href: "/#rwd-food", title: "RWD Food" },
+      { href: "/#rwd-pay", title: "RWD Pay" },
+      { id: 11, href: "/#rwd-bonus", title: "RWD Bonus" },
+      { id: 12, href: "/#rwd-goodies", title: "RWD Goodies" },
+    ],
+  },
+  {
+    title: "Drivers",
+    links: [
+      { href: "/#catchup-drivers", title: "Catch Up" },
+      { href: "/#ride-with-us", title: "Ride with us" },
+      {
+        href: "/#deliver-with-us",
+        title: "Deliver with us",
+      },
+      { href: "/#driver-hub", title: "Driver Hub" },
+      {
+        href: "/#driver-benefits",
+        title: "Driver Benefits",
+      },
+    ],
+  },
+  {
+    title: "Charity",
+    links: [
+      { href: "/#catchup-charity", title: "Catch Up" },
+      { href: "/#blog", title: "Blog" },
+      {
+        href: "/#delivered-donations",
+        title: "Delivered Donations",
+      },
+      { href: "/#road-trip", title: "Road Trip" },
+      { href: "/#road-food", title: "Road Food" },
+      { href: "/#road-health", title: "Road Health" },
+      { href: "/#road-aid", title: "Road Aid" },
+      {
+        href: "/#calamity-support",
+        title: "Calamity Support",
+      },
+    ],
+  },
+  {
+    title: "Special Delivery",
+    links: [
+      {
+        href: "/#chain-deliveries",
+        title: "Chain Deliveries",
+      },
+      {
+        href: "/#distance-deliveries",
+        title: "Distance Deliveries",
+      },
+      {
+        href: "/#private-deliveries",
+        title: "Private Deliveries",
+      },
+    ],
+  },
+  {
+    title: "Branches",
+    links: [
+      { href: "/#first-branch", title: "First Branch" },
+      { href: "/#main-branch", title: "Main Branch" },
+      { href: "/#sub-branches", title: "Sub-Branches" },
+    ],
+  },
+  {
+    title: "Terms and Policies",
+    links: [
+      { href: "/#catchyp-tap", title: "Catch Up" },
+      { href: "/#terms", title: "Terms" },
+      { href: "/#policies", title: "Policies" },
+      { href: "/#trust", title: "Trust" },
+      { href: "/#safety", title: "Safety" },
+      { href: "/#security", title: "Security" },
+      { href: "/#privacy", title: "Privacy" },
+      { href: "/#copyright", title: "Copyright" },
+    ],
+  },
+  {
+    title: "Quick Links",
+    links: [
+      {
+        href: "/#support-center",
+        title: "Support Center",
+      },
+      {
+        href: "/#car-catalogue",
+        title: "Car Catalogue",
+      },
+      { href: "/#road-updates", title: "Road Updates" },
+      {
+        href: "/#developers-only",
+        title: "Developers Only",
+      },
+    ],
+  },
+];

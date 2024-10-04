88ec316735cf39e9c8a187fc9496a24889f5b34d
Andrei <andreiwork25@gmail.com>
2024-10-04 17:42:52 +0800

    Update NavLinksDB.tsx

--- constants/NavLinksDB.tsx
+++ constants/NavLinksDB.tsx
@@ -1,7 +1,6 @@
 export const NavLinksData = [
-  { id: 1, href: "/", key: "Home", title: "Home" },
-  { id: 2, href: "/about", key: "About", title: "About" },
-  { id: 3, href: "/testimonials", key: "Testimonials", title: "Testimonials" },
-  { id: 4, href: "/pricing", key: "Pricing", title: "Pricing" },
-  { id: 5, href: "/contact", key: "Contact", title: "Contact" },
+  { href: "/", key: "Home", title: "Home" },
+  { href: "/about", key: "About", title: "About" },
+  { href: "#testimonials", key: "Testimonials", title: "Testimonials" },
+  { href: "/contact", key: "Contact", title: "Contact" },
 ];
